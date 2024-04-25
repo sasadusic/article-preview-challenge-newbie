f6caa431ec06056e4f3aa76a7863a8cd42babe69
sasadusic <sasadusic01101989@gmail.com>
2024-04-25 16:58:12 +0200

    m

---
+++ main.js
@@ -0,0 +1,5 @@
+const share = document.querySelector('.share')
+const shareModal = document.querySelector('.share-modal')
+
+share.onclick = () => shareModal.showModal()
+// share.onmouseleave = () =>  shareModal.close()
